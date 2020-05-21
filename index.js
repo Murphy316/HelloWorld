d474913408981954a0c6186bc646a94ecb337219
Qi Li (Murphy) <55805317+Murphy316@users.noreply.github.com>
2020-05-21 15:02:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+var http = require('http');
+var express = require('express');
+var app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
